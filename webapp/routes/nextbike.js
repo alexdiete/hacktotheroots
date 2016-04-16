cc45beb4fae9fa9f64cc65d4047fdf80fdef9480
Alex <alexgithub@posteo.de>
2016-04-16 15:43:12 +0200

    Nextbike - closest station

--- webapp/routes/nextbike.js
+++ webapp/routes/nextbike.js
@@ -1,24 +1,46 @@
 var request = require('request');
 var xml = require('libxmljs');
 
+function distance(s,e) {
+    var a = s.lat - e.lat;
+    var b = s.lng - e.lng;
+    return Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
+}
+
 module.exports = {
-    get_stations:  function(cb) {
+    // {lat:xxx, lng: xxx}
+    get_stations:  function(start, end,cb) {
         var stations = [];
         request('https://nextbike.net/maps/nextbike-live.xml', function(err, res, body) {
             if (err) {
                 return cb(err);
             }
             var doc = xml.parseXml(body);
+            var min_start = 9999;
+            var min_end = 9999;
+            var result = {
+                start: {},
+                end: {}
+            }
             var ma = doc.get("//city[@name='Mannheim']");
             ma.childNodes().forEach(function(e) {
-                var data = {
-                    lat: e.attr('lat').value(),
-                    lng: e.attr('lng').value(),
-                    bikes: e.attr('bikes').value(),
+                if (e.attr('bikes').value() != '0') {
+                    var data = {
+                        lat: Number.parseFloat(e.attr('lat').value()),
+                        lng: Number.parseFloat(e.attr('lng').value()),
+                        bikes: e.attr('bikes').value(),
+                    }
+                    data['distance_start'] = distance(start, data);
+                    data['distance_end'] = distance(end, data); 
+                    if (data['distance_start'] < min_start) {
+                        result.start = data;
+                    }
+                    if (data['distance_end'] < min_end) {
+                        result.end = data;
+                    }
                 }
-                stations.push(data);
             });
-            return cb(null, stations);
+            return cb(null, result);
         });
     }
 }
